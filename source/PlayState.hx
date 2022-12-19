0d4afd09f5198c03252a2038460a4df72a39856e
ShadowLOL00 <95530213+ShadowLOL00@users.noreply.github.com>
2022-12-19 12:49:22 -0300

    update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2697,7 +2697,7 @@ class PlayState extends MusicBeatState
 
 		super.update(elapsed);
 
-		scoreTxt.text = 'Score: ' + songScore + ' | Misses: ' + songMisses + ' | Rating: ' + ratingName;
+		scoreTxt.text = 'Ported by Anderson777 | Score: ' + songScore + ' | Misses: ' + songMisses + ' | Rating: ' + ratingName;
 		if(ratingName != '?')
 			scoreTxt.text += ' (' + Highscore.floorDecimal(ratingPercent * 100, 2) + '%)' + ' - ' + ratingFC;
 
