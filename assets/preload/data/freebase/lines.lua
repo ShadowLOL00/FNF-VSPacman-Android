d990829d660637566fcc4758933ce6ca9992545e
ShadowLOL00 <95530213+ShadowLOL00@users.noreply.github.com>
2022-12-20 08:22:20 -0300

    Add files via upload

---
+++ assets/preload/data/freebase/lines.lua
@@ -0,0 +1,16 @@
+function onStepHit()
+
+	if curStep == 475 then
+		triggerEvent('Play Animation', 'shit', 'dad')
+	end
+	if curStep == 544 then
+		triggerEvent('Play Animation', 'drugs', 'gf')
+		triggerEvent('Play Animation', 'no', 'bf')
+	end
+	if curStep == 576 then
+		triggerEvent('Play Animation', 'pussy', 'dad')
+	end
+	if curStep == 1056 then
+		triggerEvent('Play Animation', 'wanna', 'dad')
+	end
+	end
