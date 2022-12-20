cb53dbcbdab21cd3fa80376784ec21840cdd9eb9
ShadowLOL00 <95530213+ShadowLOL00@users.noreply.github.com>
2022-12-20 09:15:37 -0300

    update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2786,9 +2786,9 @@ class PlayState extends MusicBeatState
 				    pacSound.stop();
 				    remove(skippedText);
 				    FlxG.sound.play(Paths.sound('clickText'), 0.8)
-				  if (Conductor.songPosition < -1000 && Conductor.songPosition > 0){
-				    Conductor.songPosition = -1000;
-				  }
+				  //if (Conductor.songPosition < -1000 && Conductor.songPosition > 0){
+				   // Conductor.songPosition = -1000;
+				  //}
 				 }
 				Conductor.songPosition += FlxG.elapsed * 1000;
 					if (Conductor.songPosition >= 0)
